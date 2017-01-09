3697c572bc38f8ed8f696d5b520b575211995f5b
Davi Nascimento <davi-nasc@hotmail.com>
2017-01-09 17:05:19 -0300

    Modificado feedbacks em erros no formulário & bug de confirmação de passoword

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:full_name, :bio, :password, :email, :location)
+      params.require(:user).permit(:full_name, :bio, :password, :password_confirmation, :email, :location)
     end
 end
