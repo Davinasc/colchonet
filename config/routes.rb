3b6ee29ada7724b6d5e18617d49e36c0872dfdf9
Davi Nascimento <davi-nasc@hotmail.com>
2017-01-11 20:31:13 -0300

    Adicionado I18n e respectivas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  get 'welcome/index'
+	LOCALES = /en|pt\-BR/
 
-  resources :rooms
-  resources :users
+	match '/:locale' => 'welcome#index', locale: LOCALES, via: [:get, :post]
+  root 'welcome#index'
+
+  scope (":locale"), locale: LOCALES  do
+		resources :rooms
+		resources :users
+		resource :user_confirmation, :only => [:show]
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
